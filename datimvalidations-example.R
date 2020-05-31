4693e019ba71303115204a2a4ef2c007459c556a
kwebihaf-github <kwebihaf@gmail.com>
2020-05-31 17:36:53 +0000

    Added AGYW_PREV dataset uid and also an explanation for find and replace of the default IM in its data

--- datimvalidations-example.R
+++ datimvalidations-example.R
@@ -11,15 +11,17 @@ secrets=here::here("files","secrets.json")
 loadSecrets(secrets)
 
 # validations
+# for AGYW_PREV indicator, replace the default IM of uid=`HllvX50cXC0` with another IM of uid=`Y14uAWeaYX8` for purposes to go through validations smoothly
+# Then after running the validations,  replace uid=Y14uAWeaYX8 with uid=HllvX50cXC0
 datafile<-here::here("files","02.DATIM4U.FY2020Q2.ImportReady.TX_COMBINED.csv")
 d<-d2Parser(filename = datafile ,type = "csv")
-checkDataElementOrgunitValidity(data = d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh"))
+checkDataElementOrgunitValidity(data = d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh", "mbdbMiLZ4AA"))
 
-checkDataElementDisaggValidity(data=d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh"))
+checkDataElementDisaggValidity(data=d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh", "mbdbMiLZ4AA"))
 
 checkValueTypeCompliance(d)
 checkNegativeValues(d)
-FY2020Q2_TX_Combined_Facility_violations <-validateData(data = d, datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh"),return_violations_only = TRUE)
+FY2020Q2_TX_Combined_Facility_violations <-validateData(data = d, datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh","mbdbMiLZ4AA"),return_violations_only = TRUE)
 
 # saving of violations int csv file
 write.csv(FY2020Q2_HTS_TST_Facility_violations,file=here::here("files/violations","FY2020Q2_TX_Combined_Facility_violations.csv"), row.names = FALSE)
