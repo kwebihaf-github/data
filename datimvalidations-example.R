edcfb93dc4602b030f2c88e00c471a6958207b0a
Fred Kwebiha <kwebihaf@gmail.com>
2021-02-08 22:22:26 +0300

    modified: lines for dataset uids for MER 2.5
    Added dataset uids for MER 2.5

--- datimvalidations-example.R
+++ datimvalidations-example.R
@@ -15,13 +15,16 @@ loadSecrets(secrets)
 # Then after running the validations,  replace uid=Y14uAWeaYX8 with uid=HllvX50cXC0
 datafile<-here::here("files","02.DATIM4U.FY2020Q2.ImportReady.TX_COMBINED.csv")
 d<-d2Parser(filename = datafile ,type = "csv")
-checkDataElementOrgunitValidity(data = d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh", "mbdbMiLZ4AA"))
+#checkDataElementOrgunitValidity(data = d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh", "mbdbMiLZ4AA")) # for MER 2.4
+checkDataElementOrgunitValidity(data = d,datasets = c("TBcmmtoaCBC","BPEyzcDb8fT","zL8TlPVzEBZ","em1U5x9hhXh", "qHyrHc4zwx4")) # for MER 2.5
 
-checkDataElementDisaggValidity(data=d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh", "mbdbMiLZ4AA"))
+#checkDataElementDisaggValidity(data=d,datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh", "mbdbMiLZ4AA")) # for MER 2.4
+checkDataElementDisaggValidity(data=d,datasets = c("TBcmmtoaCBC","BPEyzcDb8fT","zL8TlPVzEBZ","em1U5x9hhXh", "qHyrHc4zwx4")) # for MER 2.5
 
 checkValueTypeCompliance(d)
 checkNegativeValues(d)
-FY2020Q2_TX_Combined_Facility_violations <-validateData(data = d, datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh","mbdbMiLZ4AA"),return_violations_only = TRUE)
+#FY2020Q2_TX_Combined_Facility_violations <-validateData(data = d, datasets = c("qzVASYuaIey","jKdHXpBfWop","BPEyzcDb8fT","em1U5x9hhXh","mbdbMiLZ4AA"),return_violations_only = TRUE) # for MER 2.4
+FY2020Q2_TX_Combined_Facility_violations <-validateData(data = d, datasets = c("TBcmmtoaCBC","BPEyzcDb8fT","zL8TlPVzEBZ","em1U5x9hhXh", "qHyrHc4zwx4"),return_violations_only = TRUE) # for MER 2.4
 
 # saving of violations int csv file
 write.csv(FY2020Q2_HTS_TST_Facility_violations,file=here::here("files/violations","FY2020Q2_TX_Combined_Facility_violations.csv"), row.names = FALSE)
